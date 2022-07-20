8fc86a3f7ae40673d410ad4d465880c9bb226872
CarolinaArlone <carolinaarlone@gmail.com>
2022-07-20 11:33:17 +0200

    Begin Cloudinary

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,7 +4,7 @@ router.use("/cars", require('./cars.routes'))
 router.use("/booking", require('./booking.routes'))
 router.use("/user", require('./user.routes'))
 router.use("/auth", require('./auth.routes'))
-
+router.use("/upload", require('./upload.routes'))
 router.use('/', require('./test.routes'))
 
 module.exports = router
---
+++ routes/upload.routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router
+const uploader = require('./../config/cloudinary.config')
+
+//poner lo que tiene Germán
+
+module.exports = router
